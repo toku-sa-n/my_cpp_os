39b150c5600c107a12cd9233bb9a922a2351dd4e
toku-sa-n <tokuchan3515@gmail.com>
2019-10-13 11:02:24 +0900

    [Change]Draw stripes on the screen

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ void write_to_memory(int address, int data);
 void os_main()
 {
     for (int i = 0xa0000; i <= 0xaffff; i++) {
-        write_to_memory(i, 15);
+        write_to_memory(i, i & 0x0f);
     }
 
     while (1) {
