6dec9ed44996c82ac6b988e9d111d79ac6d08a4e
toku-sa-n <tokuchan3515@gmail.com>
2019-10-13 11:01:30 +0900

    [Add]Make the screen white

--- asm_func.asm
+++ asm_func.asm
@@ -1,8 +1,15 @@
 [bits 32]
     global io_hlt
+    global write_to_memory
 
 [section .text]
 
 io_hlt:
     hlt
+    ret
+
+write_to_memory:            ; void write_to_memory(int address, int data);
+    mov ecx,[esp+4]
+    mov al,[esp+8]
+    mov [ecx],al
     ret
--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 extern "C" {
 void io_hlt();
+void write_to_memory(int address, int data);
 }
 
 void os_main()
 {
-fin:
-    io_hlt();
-    goto fin;
+    for (int i = 0xa0000; i <= 0xaffff; i++) {
+        write_to_memory(i, 15);
+    }
+
+    while (1) {
+        io_hlt();
+    }
 }
