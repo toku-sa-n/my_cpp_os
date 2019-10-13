2b637a59b037de170fb28fe045d6a3af6672342e
toku-sa-n <tokuchan3515@gmail.com>
2019-10-13 18:31:42 +0900

    [Add]Set color palette

--- asm_func.asm
+++ asm_func.asm
@@ -1,8 +1,85 @@
 [bits 32]
     global io_hlt
+    global io_cli
+    global io_sti
+    global io_stihlt
+
+    global io_in8
+    global io_in16
+    global io_in32
+
+    global io_out8
+    global io_out16
+    global io_out32
+
+    global io_load_eflags
+    global io_store_eflags
 
 [section .text]
 
 io_hlt:
     hlt
+    ret
+
+io_cli:
+    cli
+    ret
+
+io_sti:
+    sti
+    ret
+
+io_stihlt:
+    sti
+    hlt
+    ret
+
+; int io_in(8|16|32)(int port);
+io_in8:
+    mov edx,[esp+4]
+    mov eax,0
+    in al,dx
+    ret
+
+io_in16:
+    mov edx,[esp+4]
+    mov eax,0
+    in ax,dx
+    ret
+
+io_in32:
+    mov edx,[esp+4]
+    in eax,dx
+    ret
+
+; void io_out(8|16|32)(int port, int data);
+io_out8:
+    mov edx,[esp+4]
+    mov al,[esp+8]
+    out dx,al
+    ret
+
+io_out16:
+    mov edx,[esp+4]
+    mov eax,[esp+8]
+    out dx,ax
+    ret
+
+io_out32:
+    mov edx,[esp+4]
+    mov eax,[esp+8]
+    out dx,eax
+    ret
+
+; int io_load_eflags();
+io_load_eflags:
+    pushfd
+    pop eax
+    ret
+
+; void io_store_eflags(int eflags);
+io_store_eflags:
+    mov eax,[esp+4]
+    push eax
+    popfd
     ret
--- main.cpp
+++ main.cpp
@@ -1,15 +1,70 @@
 extern "C" {
 void io_hlt();
+void io_cli();
+void io_out8(int port, int data);
+int io_load_eflags();
+void io_store_eflags(int eflags);
+void os_main();
 }
+void set_palette(int start, int end, unsigned char* rgb);
+void init_palette();
 
+// Don't move os_main because this function must come after the label `bootpack:' which is in head.asm
 void os_main()
 {
-    for (int i = 0xa0000; i <= 0xaffff; i++) {
-        char* p = (char*)i;
-        *p = i & 0x0f;
+    init_palette();
+
+    char* p = (char*)0xa0000;
+
+    for (int i = 0; i <= 0xffff; i++) {
+        p[i] = i & 0x0f;
     }
 
     while (1) {
         io_hlt();
     }
+}
+
+void set_palette(int start, int end, unsigned char* rgb)
+{
+    // See: http://oswiki.osask.jp/?VGA
+    // ビデオDAコンバータの項
+    int eflags = io_load_eflags();
+    io_cli();
+    io_out8(0x03c8, start);
+    for (int i = start; i <= end; i++) {
+        // You can specify the number between 0 and 63
+        // as R, G and B so divide by 4 because the range of each elements of the array rgb is from 0 to 255.
+        io_out8(0x03c9, rgb[0] / 4);
+        io_out8(0x03c9, rgb[1] / 4);
+        io_out8(0x03c9, rgb[2] / 4);
+        rgb += 3;
+    }
+    io_store_eflags(eflags);
+}
+
+void init_palette()
+{
+    static unsigned char rgb_table[16 * 3] = {
+        // clang-format off
+        0x00, 0x00, 0x00,
+        0xff, 0x00, 0x00,
+        0x00, 0xff, 0x00,
+        0xff, 0xff, 0x00,
+        0x00, 0x00, 0xff,
+        0xff, 0x00, 0xff,
+        0x00, 0xff, 0xff,
+        0xff, 0xff, 0xff,
+        0xc6, 0xc6, 0xc6,
+        0x84, 0x00, 0x00,
+        0x00, 0x84, 0x00,
+        0x84, 0x84, 0x00,
+        0x00, 0x00, 0x84,
+        0x84, 0x00, 0x84,
+        0x00, 0x84, 0x84,
+        0x84, 0x84, 0x84,
+        // clang-format on
+    };
+
+    set_palette(0, 15, rgb_table);
 }
