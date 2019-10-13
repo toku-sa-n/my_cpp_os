78b3b75131f93fb50f31d0822753e4d375a14867
toku-sa-n <tokuchan3515@gmail.com>
2019-10-13 11:12:33 +0900

    [Refactoring]Write data to the memory using pointer
    Delete write_to_memory function in asm_func.asm

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 extern "C" {
 void io_hlt();
-void write_to_memory(int address, int data);
 }
 
 void os_main()
 {
     for (int i = 0xa0000; i <= 0xaffff; i++) {
-        write_to_memory(i, i & 0x0f);
+        char* p = (char*)i;
+        *p = i & 0x0f;
     }
 
     while (1) {
