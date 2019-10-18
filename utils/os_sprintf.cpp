56ed5bba5323a2d72f5a1c2612551e5665969a3f
toku-sa-n <tokuchan3515@gmail.com>
2019-10-18 21:09:21 +0900

    [Add]%d format for one-digit numbers

--- utils/os_sprintf.cpp
+++ utils/os_sprintf.cpp
@@ -1,18 +1,48 @@
 #include <cstring>
+#include <iostream>
 #include <stdarg.h>
+
+static int int_to_chars(char* str, int n)
+{
+    char buf[1024] = { '\0' };
+
+    int ptr = 0;
+    int digits = 0;
+    while (n > 0) {
+        buf[ptr++] = n % 10 + '0';
+        n /= 10;
+        digits++;
+    }
+
+    for (int i = 0; i < ptr / 2; i++) {
+        char temp = buf[i];
+        buf[i] = buf[ptr - 1 - i];
+        buf[ptr - 1 - i] = temp;
+    }
+
+    for (int i = 0; i < ptr; i++) {
+        *str++ = buf[i];
+    }
+
+    return digits;
+}
 
 int os_vsprintf(char* str, const char* format, va_list ap)
 {
     int count = 0;
 
-    int format_len = strlen(format);
-    for (int i = 0; i < format_len; i++) {
-        str[i] = format[i];
-        count++;
+    while (*format != '\0') {
+        if (*format != '%') {
+            *str++ = *format++;
+            count++;
+            continue;
+        }
+
+        format += 2;
+        count += int_to_chars(str++, va_arg(ap, int));
     }
 
-    str[format_len] = '\0';
-
+    *str = '\0';
     return count;
 }
 
