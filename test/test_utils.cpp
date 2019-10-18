214d20a6ee9d316705ddafe9d8a8e0a1996f1fd1
toku-sa-n <tokuchan3515@gmail.com>
2019-10-18 17:09:44 +0900

    [Fix]Display the wrong expected and actual values

--- test/test_utils.cpp
+++ test/test_utils.cpp
@@ -20,24 +20,24 @@ bool test_os_vsprintf(const char* format, ...)
     int builtin_count = vsprintf(builtin_buf, format, ap);
     va_end(ap);
 
-    auto error_msg = [=](std::string message) {
+    auto error_msg = [=](std::string message, auto expected, auto actual) {
         std::cout << std::endl;
         std::cout << "--------------------------------" << std::endl;
         std::cout << message << std::endl;
         std::cout << "  Format: " << format << std::endl;
         std::cout << std::endl;
-        std::cout << "Expected: " << builtin_count << std::endl;
-        std::cout << "  Actual: " << os_count << std::endl;
+        std::cout << "Expected: " << expected << std::endl;
+        std::cout << "  Actual: " << actual << std::endl;
         std::cout << "--------------------------------" << std::endl;
     };
 
     if (os_count != builtin_count) {
-        error_msg("Wrong return value!");
+        error_msg("Wrong return value!", builtin_count, os_count);
         return false;
     }
 
     if (strcmp(os_buf, builtin_buf)) {
-        error_msg("Test failed!");
+        error_msg("Test failed!", builtin_buf, os_buf);
         return false;
     }
 
