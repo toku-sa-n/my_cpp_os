2ac69a56105a62a5cca146e4a47b8a47c78e0cd9
toku-sa-n <tokuchan3515@gmail.com>
2019-10-18 14:13:49 +0900

    [Add]os_sprintf func

--- utils/os_sprintf.cpp
+++ utils/os_sprintf.cpp
@@ -1,5 +1,18 @@
+#include <cstring>
 #include <stdarg.h>
 
 int os_vsprintf(char* str, const char* format, va_list ap)
 {
+}
+
+int os_sprintf(char* str, const char* format, ...)
+{
+    va_list ap;
+    va_start(ap, format);
+
+    int count = os_vsprintf(str, format, ap);
+
+    va_end(ap);
+
+    return count;
 }
