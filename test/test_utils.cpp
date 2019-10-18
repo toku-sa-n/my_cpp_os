1e73458a196ee43c89dc54e6c76691108c74edb3
toku-sa-n <tokuchan3515@gmail.com>
2019-10-18 17:06:15 +0900

    [Change]Align vertical line of test output

--- test/test_utils.cpp
+++ test/test_utils.cpp
@@ -24,10 +24,10 @@ bool test_os_vsprintf(const char* format, ...)
         std::cout << std::endl;
         std::cout << "--------------------------------" << std::endl;
         std::cout << message << std::endl;
-        std::cout << "Format: " << format << std::endl;
+        std::cout << "  Format: " << format << std::endl;
         std::cout << std::endl;
         std::cout << "Expected: " << builtin_count << std::endl;
-        std::cout << "Actual: " << os_count << std::endl;
+        std::cout << "  Actual: " << os_count << std::endl;
         std::cout << "--------------------------------" << std::endl;
     };
 
