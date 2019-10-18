8416b6921fa1b8a0d3d7932efe7fc3da5f9067bf
toku-sa-n <tokuchan3515@gmail.com>
2019-10-18 14:30:07 +0900

    [Add]os_vsprintf for non-formt string

--- utils/os_sprintf.cpp
+++ utils/os_sprintf.cpp
@@ -3,6 +3,17 @@
 
 int os_vsprintf(char* str, const char* format, va_list ap)
 {
+    int count = 0;
+
+    int format_len = strlen(format);
+    for (int i = 0; i < format_len; i++) {
+        str[i] = format[i];
+        count++;
+    }
+
+    str[format_len] = '\0';
+
+    return count;
 }
 
 int os_sprintf(char* str, const char* format, ...)
