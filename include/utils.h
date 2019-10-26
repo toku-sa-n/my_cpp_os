1a5d68f8515960f23e301279956f4d9fd63292ff
toku-sa-n <tokuchan3515@gmail.com>
2019-10-26 23:44:01 +0900

    [Change]Declare class methods inside header file
    See http://www.cplusplus.com/forum/general/113904/#msg622073arti

--- include/utils.h
+++ include/utils.h
@@ -13,4 +13,39 @@ class Queue {
     int CountElements();
 };
 
+template <unsigned QueueSize>
+bool Queue<QueueSize>::Enqueue(unsigned char data)
+{
+    if (num_free_ == 0) {
+        flags_ |= kFlagsOverRun_;
+        return false;
+    }
+
+    buf_[next_pos_write_] = data;
+    next_pos_write_ = (next_pos_write_ + 1) % QueueSize;
+
+    num_free_--;
+    return true;
+}
+
+template <unsigned QueueSize>
+int Queue<QueueSize>::Dequeue()
+{
+    if (num_free_ == QueueSize) {
+        return false;
+    }
+
+    int data = buf_[next_pos_read_];
+    next_pos_read_ = (next_pos_read_ + 1) % QueueSize;
+
+    num_free_++;
+    return next_pos_read_;
+}
+
+template <unsigned QueueSize>
+int Queue<QueueSize>::GetNumElements()
+{
+    return QueueSize - num_free_;
+}
+
 int OSSPrintf(char* str, const char* format, ...);
