36d2ac53c1ca78b90342ea86f0766691a238efd1
toku-sa-n <tokuchan3515@gmail.com>
2019-11-01 07:35:45 +0900

    [Change]Mark Between func as inline

--- include/utils.h
+++ include/utils.h
@@ -55,7 +55,7 @@ int OSSPrintf(char* str, const char* format, ...);
 int OSVSPrintf(char* str, const char* format, va_list ap);
 
 template <typename T>
-T Between(const T value, const T from, const T to)
+inline T Between(const T value, const T from, const T to)
 {
     if (value < from) {
         return from;
